465c2bd9bb7da9b0767f40405886731ffe9fd823
Andy Chen <andy.chen@entyrecare.com>
2026-03-04 11:34:53 -0500

    cron job automate status check

--- real_time_eligibility_check/2.batch_status_check.py
+++ real_time_eligibility_check/2.batch_status_check.py
@@ -1,8 +1,17 @@
 import requests
+import csv
 
-''' this is pulled from MA_medicaid_eligibility_results.csv'''
-batchid = "019cb3fe-d505-7a22-a73b-6e791940c03c"
+# Path to the CSV file
+csv_path = "/Users/Andy.Chen/Billing_Automation/eligibility_batch_log.csv"
 
+''' Read the last row's first column from csv_path '''
+batchid = None
+with open(csv_path, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = list(reader)
+    batchid = rows[-1][0]  # last row, first column
+
+print(batchid)
 
 url = f"https://manager.us.stedi.com/2024-04-01/eligibility-manager/batch/{batchid}"
 response = requests.request("GET", url, headers = {
